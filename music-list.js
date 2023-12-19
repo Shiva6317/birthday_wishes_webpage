7f7aeb6291ffeaa1cfac354b4dcc3cac7c9ce4d2
Sanjay Mishra <shivjeetpaswan86@gmail.com>
2023-12-19 18:25:21 +0530

    song

--- music-list.js
+++ music-list.js
@@ -1,64 +1,64 @@
 let allMusic = [
-    {
-        name: "Shape-of-You",
-        artist: "Ed Sheeran",
-        img: "music-4",
-        src: "music-1"
-    },
-    {
-        name: "Calm-Down",
-        artist: "Rema, Selena Gomez",
-        img: "music-4",
-        src: "music-2"
-    },
-    {
-        name: "Let Me Down Slowly",
-        artist: "Alec Benjamin",
-        img: "music-4",
-        src: "music-3"
-    },
+    // {
+    //     name: "Shape-of-You",
+    //     artist: "Ed Sheeran",
+    //     img: "music-4",
+    //     src: "music-1"
+    // },
+    // {
+    //     name: "Calm-Down",
+    //     artist: "Rema, Selena Gomez",
+    //     img: "music-4",
+    //     src: "music-2"
+    // },
+    // {
+    //     name: "Let Me Down Slowly",
+    //     artist: "Alec Benjamin",
+    //     img: "music-4",
+    //     src: "music-3"
+    // },
     {
         name: "Love Me Like You Do",
         artist: "Ellie Goulding",
         img: "music-4",
         src: "music-4"
     },
-    {
-        name: "Starboy",
-        artist: "The Weeknd",
-        img: "music-6",
-        src: "music-5"
-    },
-    {
-        name: "Girls Like You",
-        artist: "Maroon 5",
-        img: "music-6",
-        src: "music-6"
-    },
-    {
-        name: "On My Way",
-        artist: "Alan Walker",
-        img: "music-6",
-        src: "music-7"
-    },
-    {
-        name: "Sorry",
-        artist: "Justin Beiber",
-        img: "music-6",
-        src: "music-8"
-    },
-    {
-        name: "Sugar and Brownies",
-        artist: "Dharia",
-        img: "music-6",
-        src: "music-9"
-    },
-    {
-        name: "Faded",
-        artist: "Alan Walker",
-        img: "music-6",
-        src: "music-10"
-    },
+    // {
+    //     name: "Starboy",
+    //     artist: "The Weeknd",
+    //     img: "music-6",
+    //     src: "music-5"
+    // },
+    // {
+    //     name: "Girls Like You",
+    //     artist: "Maroon 5",
+    //     img: "music-6",
+    //     src: "music-6"
+    // },
+    // {
+    //     name: "On My Way",
+    //     artist: "Alan Walker",
+    //     img: "music-6",
+    //     src: "music-7"
+    // },
+    // {
+    //     name: "Sorry",
+    //     artist: "Justin Beiber",
+    //     img: "music-6",
+    //     src: "music-8"
+    // },
+    // {
+    //     name: "Sugar and Brownies",
+    //     artist: "Dharia",
+    //     img: "music-6",
+    //     src: "music-9"
+    // },
+    // {
+    //     name: "Faded",
+    //     artist: "Alan Walker",
+    //     img: "music-6",
+    //     src: "music-10"
+    // },
     {
         name:"Kal Ho Na Ho",
         artist: "Arijit Singh",
